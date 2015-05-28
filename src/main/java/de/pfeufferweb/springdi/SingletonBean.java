43f9b692eb531a362676289606408b5fd3717e24
René Pfeuffer <rene@pfeuffer-web.de>
2015-05-28 10:51:35 +0200

    demo with application context

--- src/main/java/de/pfeufferweb/springdi/SingletonBean.java
+++ src/main/java/de/pfeufferweb/springdi/SingletonBean.java
@@ -1,17 +1,24 @@
 package de.pfeufferweb.springdi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SingletonBean {
-    @Autowired private PrototypeBean prototype;
+    @Autowired
+    private ApplicationContext context;
+
     public SingletonBean() {
         System.out.println("creating singleton bean");
     }
 
     @Override
     public String toString() {
-        return "Singleton with " + prototype;
+        return "Singleton with " + getPrototype();
+    }
+
+    private PrototypeBean getPrototype() {
+        return context.getBean(PrototypeBean.class);
     }
 }
