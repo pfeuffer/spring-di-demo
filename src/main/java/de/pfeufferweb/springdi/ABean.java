6ebb4e01db4c97e049053fd79da7229f6e2a1018
René Pfeuffer <rene@pfeuffer-web.de>
2015-05-28 09:27:06 +0200

    demo for simple prototype

--- src/main/java/de/pfeufferweb/springdi/ABean.java
+++ src/main/java/de/pfeufferweb/springdi/ABean.java
@@ -1,9 +1,15 @@
 package de.pfeufferweb.springdi;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
 public class ABean {
+    public ABean() {
+        System.out.println("creating a bean");
+    }
+
     @Override
     public String toString() {
         return "Hallo";
--- src/main/java/de/pfeufferweb/springdi/SpringDI.java
+++ src/main/java/de/pfeufferweb/springdi/SpringDI.java
@@ -7,8 +7,10 @@ public class SpringDI {
     public static void main(String[] args) throws InterruptedException {
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"application-context.xml"});
 
-        ABean bean = context.getBean(ABean.class);
+        for (int i = 0; i < 5; ++i) {
+            ABean bean = context.getBean(ABean.class);
 
-        System.out.println(bean);
+            System.out.println(bean);
+        }
     }
 }
